cab20fa9ca53b516041337d32f1a7f73d1b13bb6
tensor <drew.tensor@gmail.com>
2022-05-02 07:35:49 -0700

    EEA added

--- binpow.cpp
+++ binpow.cpp
@@ -11,6 +11,19 @@ long long binpow(long long a, long long b) {
     return res;
 }
 
-int gcd(int a, int b) {
-    return b ? gcd (b, a % b) : a;
+int gcd(int a, int b, int& x, int& y) {
+    x = 1, y = 0;
+    int x1 = 0, y1 = 1, a1 = a, b1 = b;
+    while (b1) {
+        int q = a1 / b1;
+        tie(x, x1) = make_tuple(x1, x - q * x1);
+        tie(y, y1) = make_tuple(y1, y - q * y1);
+        tie(a1, b1) = make_tuple(b1, a1 - q * b1);
+    }
+    return a1;
 }
+
+int lcm(int a, int b) {
+    return a / gcd(a, b) * b;
+}
+
